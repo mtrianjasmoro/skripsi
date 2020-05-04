011d6cc40e7491afaedac43f4f5270fad25b858f
mtrianjasmoro <41131706+mtrianjasmoro@users.noreply.github.com>
2020-05-04 21:03:44 +0700

    Add files via upload
    tambah komentar eksternal+analisa sentimen. rubah desain. hapus kernel SVM dari UI, kernel svm dari system. proposal merubah caption grafik dan gambar.

--- svmnbc.py
+++ svmnbc.py
@@ -25,10 +25,10 @@ for i in range(0, len(dataset)):
     review = review.lower()
     review = review.split()    
     # Menghilangkan kata yang tidak ada di stopwords
-#     psi = StemmerFactory()
-#     ps = psi.create_stemmer()
-#     review = [ps.stem(word) for word in review if not word in indo]
-    # print(i)
+    psi = StemmerFactory()
+    ps = psi.create_stemmer()
+    review = [ps.stem(word) for word in review if not word in indo]
+    print(i)
     review = ' '.join(review)
     corpus.append(review)
 class Analis:
@@ -39,26 +39,56 @@ class Analis:
         articles=np.array(corpus)
         labels=np.array(dataset['sentimen'])
 
-        tf_vectorizer=TfidfVectorizer(min_df =4,max_df=0.3,ngram_range=(1,3))
-        x_train_tfidf=tf_vectorizer.fit_transform(articles).toarray()
-        x_test_tfidf=tf_vectorizer.transform(articles)
+        self.tf_vectorizer=TfidfVectorizer(min_df =4,max_df=0.3,ngram_range=(1,3))
+        x_train_tfidf=self.tf_vectorizer.fit_transform(articles).toarray()
+        x_test_tfidf=self.tf_vectorizer.transform(articles)
 
-        X_train, X_test, y_train, y_test=train_test_split(x_train_tfidf,labels,test_size = training,random_state = 0)
         self.X_train, self.X_test, self.y_train, self.y_test=train_test_split(x_train_tfidf,labels,test_size = training,random_state = 0)
-
-        
-    def svm(self,kernel):
-        classifierSVM = SVC(kernel = kernel, random_state = 0, gamma='auto')
-        classifierSVM.fit(self.X_train, self.y_train)
-        y_pred_SVM = classifierSVM.predict(self.X_test)
-        
+                
+    def svm(self):                
         svm_hasil = []
-        svm_hasil.append(accuracy_score(self.y_test, y_pred_SVM, normalize=True))
-        svm_hasil.append(recall_score(self.y_test, y_pred_SVM, average='micro'))
-        svm_hasil.append(precision_score(self.y_test, y_pred_SVM, average='micro'))
+        kernel=["linear","poly","sigmoid","rbf"]
+        for i in kernel:
+            classifierSVM = SVC(kernel = i, random_state = 0, gamma='auto')
+            classifierSVM.fit(self.X_train, self.y_train)
+            y_pred_SVM = classifierSVM.predict(self.X_test)
+            svm_hasil.append(accuracy_score(self.y_test, y_pred_SVM, normalize=True))
+            svm_hasil.append(recall_score(self.y_test, y_pred_SVM, average='micro'))
+            svm_hasil.append(precision_score(self.y_test, y_pred_SVM, average='micro'))
         
         return svm_hasil
-
+    
+    def kal_eks(self,kalimat):
+        cc = []
+        #preprosesing
+        review = re.sub('[^a-zA-Z]', ' ', kalimat)
+        review = review.lower()
+        review = review.split()    
+        # Menghilangkan kata yang tidak ada di stopwords
+        psi = StemmerFactory()
+        ps = psi.create_stemmer()
+        review = [ps.stem(word) for word in review if not word in indo]
+        review = ' '.join(review)
+        cc.append(review)
+        aa=np.array(cc)
+        xx=self.tf_vectorizer.transform(aa).toarray()
+        
+        pred_eks=[]
+        
+        #svm
+        kernel=["linear","poly","sigmoid","rbf"]
+        for i in kernel:            
+            psvm = SVC(kernel = i, random_state = 0, gamma='auto')
+            psvm.fit(self.X_train, self.y_train)
+            pred_eks.append(psvm.predict(xx))
+        
+        #nbc
+        pnbc = GaussianNB()
+        pnbc.fit(self.X_train, self.y_train)
+        pred_eks.append(pnbc.predict(xx))
+        
+        return pred_eks
+                    
     def nbc(self):
         classifierNB = GaussianNB()
         classifierNB.fit(self.X_train, self.y_train)
